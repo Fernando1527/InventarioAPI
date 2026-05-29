06c75e02839bac0d3e3666a7330957c9e563b753
Fernando Abrego <124549677+Fernando1527@users.noreply.github.com>
2026-05-29 17:51:54 -0600

    Nuevo cambio

---
+++ InventarioWeb/Controllers/CategoriasController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using InventarioWeb.Models;
+using System.Text.Json;
+
+namespace InventarioWeb.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        public CategoriasController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient();
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var response = await _httpClient.GetAsync("https://localhost:7058/api/Categorias");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return View(new List<Categoria>());
+            }
+
+            var json = await response.Content.ReadAsStringAsync();
+
+            var categorias = JsonSerializer.Deserialize<List<Categoria>>
+            (
+                json,
+                new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                }
+            );
+
+            return View(categorias);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
---
+++ InventarioWeb/Controllers/ProductosController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InventarioWeb.Controllers
+{
+    public class ProductosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
