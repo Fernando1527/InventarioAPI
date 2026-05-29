7a6bf0e233c18b7d96fdfc69d226d3ae2df43d45
Fernando Abrego <124549677+Fernando1527@users.noreply.github.com>
2026-05-29 17:59:32 -0600

    Nuevo

--- InventarioWeb/Controllers/ProductosController.cs
+++ InventarioWeb/Controllers/ProductosController.cs
@@ -1,12 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using InventarioWeb.Models;
+using System.Text.Json;
 
 namespace InventarioWeb.Controllers
 {
     public class ProductosController : Controller
     {
-        public IActionResult Index()
+        private readonly HttpClient _httpClient;
+
+        public ProductosController(IHttpClientFactory httpClientFactory)
         {
-            return View();
+            _httpClient = httpClientFactory.CreateClient();
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var response = await _httpClient.GetAsync("https://localhost:7058/api/Productos");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return View(new List<Producto>());
+            }
+
+            var json = await response.Content.ReadAsStringAsync();
+
+            var productos = JsonSerializer.Deserialize<List<Producto>>
+            (
+                json,
+                new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                }
+            );
+
+            return View(productos);
         }
 
         public IActionResult Create()
---
+++ InventarioWeb/Models/Producto.cs
@@ -0,0 +1,17 @@
+﻿namespace InventarioWeb.Models
+{
+    public class Producto
+    {
+        public int IdProducto { get; set; }
+
+        public string Nombre { get; set; } = string.Empty;
+
+        public string Descripcion { get; set; } = string.Empty;
+
+        public decimal Precio { get; set; }
+
+        public int Cantidad { get; set; }
+
+        public int IdCategoria { get; set; }
+    }
+}
